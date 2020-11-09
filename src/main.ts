6ff602ef5d15b130860fcaaa953f8d8d1c5c466e
euberdeveloper <euberdeveloper@gmail.com>
2020-11-09 21:12:33 +0100

    feat(main.ts): add start server script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log(0);
+import 'module-alias/register';
+
+import * as express from 'express';
+import * as http from 'http';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import { httpsRedirect } from './utils/httpsRedirect';
+
+import CONFIG from '@/config';
+import router from '@/router';
+
+const app = express();
+
+app.use(compression());
+if (process.env.NODE_ENV === 'production') {
+    app.use(httpsRedirect);
+}
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api', router());
+app.use(express.static(CONFIG.SETTINGS.PUBLIC_PATH));
+
+http.createServer(app).listen(CONFIG.SERVER.PORT);
+console.log(`Http listening on port ${CONFIG.SERVER.PORT}`);

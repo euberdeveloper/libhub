a94ad77a946fac5348babd8d95a89304311285d7
euberdeveloper <euberdeveloper@gmail.com>
2020-11-09 22:23:40 +0100

    feat(router): add router skeleton
    Router skeleton is made with a smart way, using dree and a directory structure

---
+++ src/router/v1/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { scan } from 'dree';
+import * as path from 'path';
+
+export default function(): Router {
+    const router = Router();
+
+    scan(path.join(__dirname, 'routes'), { extensions: ['js'] }, file => {
+        if (file.name.includes('.route')) {
+            require(file.path).route(router);
+        }
+    });
+
+    return router;
+}

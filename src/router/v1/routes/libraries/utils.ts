33e065365abdc591577af64b2adaa6c851a06744
sebdisdev <seba.dissegna@gmail.com>
2020-11-11 16:55:48 +0100

    fix(router): fix libraries routes

---
+++ src/router/v1/routes/libraries/utils.ts
@@ -0,0 +1,56 @@
+import * as Joi from 'joi';
+
+import { ApiPatchLibrariesLidBody, ApiPostLibrariesBody, ApiPutLibrariesLidBody } from '@/types/api/libraries';
+
+export function validatePostOrPutLibraries(body: any): boolean {
+    const schema = Joi.object({
+        name: Joi
+            .string()
+            .min(1)
+            .max(1000)
+            .required()        
+    })
+    .required()
+    .unknown();
+
+    return schema.validate(body).error === undefined;
+}
+
+export function purgePostLibraries(body: ApiPostLibrariesBody): ApiPostLibrariesBody {
+    return {
+        name: body.name,
+        owners: [],
+        schema: {
+            resources: [],
+            ubications: []
+        }
+    };
+}
+
+export function purgePutLibraries(body: ApiPutLibrariesLidBody): ApiPutLibrariesLidBody {
+    return {
+        name: body.name
+    };
+}
+
+export function validatePatchLibraries(body: any): boolean {
+    const schema = Joi.object({
+        name: Joi
+            .string()
+            .min(1)
+            .max(1000)
+            .optional()        
+    })
+    .required()
+    .unknown();
+   
+    return schema.validate(body).error === undefined;
+}
+
+export function purgePatchLibraries(body: ApiPatchLibrariesLidBody): ApiPatchLibrariesLidBody {
+    return Object.keys(body)
+        .filter(key => [
+            'name'
+        ].includes(key))
+        .reduce<ApiPatchLibrariesLidBody>((acc, curr) => ({...acc, [curr]: body[curr]}), {});
+}

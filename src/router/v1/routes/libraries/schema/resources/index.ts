ab65e68343e3e0795a125e792c70d2085ebcd476
x3n0peltis <federico.stefini@studenti.unitn.it>
2020-11-13 15:25:32 +0100

    feat(types): added schema resources

---
+++ src/router/v1/routes/libraries/schema/resources/index.ts
@@ -0,0 +1,3 @@
+export type ApiGetLibrariesLidSchemaResources = string[];
+export type ApiPostLibrariesLidSchemaResourcesBody = FormData;
+export type ApiPostLibrariesLidSchemaResourcesResult = string;

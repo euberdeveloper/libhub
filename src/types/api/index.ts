95dac15e85ab75625fa1f6c167fb29ceeac50258
euberdeveloper <euberdeveloper@gmail.com>
2020-11-16 16:33:54 +0100

    fix(type): fix import problem

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,2 +1,6 @@
 export * from './error';
 export * from './libraries';
+export * from './libraries/books';
+export * from './libraries/schema';
+export * from './libraries/schema/resources';
+export * from './libraries/schema/ubications';

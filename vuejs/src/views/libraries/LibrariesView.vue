45b396ea288f28b97cb4489bb7a8e0eb05c74559
CarloZanotto <carlo.zanotto@studenti.unitn.it>
2020-11-18 15:48:03 +0100

    feat(vuejs): add labels view

---
+++ vuejs/src/views/libraries/LibrariesView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="libraries-view">
+    <libraries />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Libraries from "@/components/libraries/Libraries.vue";
+@Component({
+  components: { Libraries },
+})
+export default class LibrariesView extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.libraries-view {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ vuejs/src/views/libraries/lid/LibrariesLidView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="libraries-lid-view">
+    <libraries-lid :lid="lid" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import LibrariesLid from "@/components/libraries/lid/LibrariesLid.vue";
+
+@Component({
+  components: { LibrariesLid },
+})
+export default class LibrariesLidView extends Vue {
+  /* PROPS */
+
+  @Prop({ type: String, required: true })
+  lid!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+.libraries-lid-view {
+  width: 100%;
+  height: 100%;
+}
+</style>

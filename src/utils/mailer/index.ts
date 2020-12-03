56a6615d59e7835c6e71177863d1a4cd4b6b2d61
CarloZanotto <carlo.zanotto@studenti.unitn.it>
2020-12-03 17:33:23 +0100

    feat(mailer): add mailer send function

--- src/utils/mailer/index.ts
+++ src/utils/mailer/index.ts
@@ -22,3 +22,22 @@ const mailer = createTransport({
 mailer.use('compile', pugEngine({
     templateDir: CONFIG.EMAIL.TEMPLATES_PATH
 }));
+
+/**
+ * Sends an email
+ * @param to The recipient or recipients of the email
+ * @param subject The subject of the email
+ * @param template The pug template of the email
+ * @param ctx The context of the email
+ * @param attachments The attachments of the email
+ */
+export async function sendEmail(to: string | string[], subject: string, template: EmailTemplates, ctx: any, attachments: any[] = []): Promise<void> {
+    await mailer.sendMail({
+        from: 'info@ghostmakers.it',
+        to,
+        subject,
+        template,
+        ctx,
+        attachments
+    } as SendMailOptions);
+}

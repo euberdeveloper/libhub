abc7754a16b080f7cb045f922feedbea895ade9b
euberdeveloper <euberdeveloper@gmail.com>
2020-12-03 12:23:19 +0100

    feat: add functions to fs-async

--- src/utils/fs-async/index.ts
+++ src/utils/fs-async/index.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
 import * as util from 'util';
 
-export const rename = util.promisify(fs.rename);
-export const unlink = util.promisify(fs.unlink);
 export const exists = util.promisify(fs.exists);
+export const writeFile = util.promisify(fs.writeFile);
+export const rename = util.promisify(fs.rename);
 export const mkdir = util.promisify(fs.mkdir);
 export const copyFile = util.promisify(fs.copyFile);
+export const unlink = util.promisify(fs.unlink);

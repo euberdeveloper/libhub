4342af43a692aa4581658fcfe1b4012328cb881e
euberdeveloper <euberdeveloper@gmail.com>
2020-12-10 10:19:25 +0100

    feat(routes):  ref labels

--- src/router/routes/users/libraries/labels/labels.route.ts
+++ src/router/routes/users/libraries/labels/labels.route.ts
@@ -14,5 +14,30 @@ import { validatePostOrPutLabels, purgePostLabels, purgePutLabels, validatePatch
 
 export function route(router: Router): void {
 
+    router.get('/user/:uid/libraries/:lid/labels', auth('uid'), validateDbId('uid'), async (req: Request & ReqAuthenticated & ReqIdParams, res) => {
+        await aceInTheHole(res, async () => {
+            const user = req.user;
+            const lid = req.idParams;
+
+            const found = await dbQuery<boolean>(async db => {
+                const library = await db.collection(DBCollections.LIBRARIES).countDocuments({ _id: lid, owners: user._id });
+                return library > 0;
+            });
+            if (!found) {
+                const err: ApiError = {
+                    message: 'Library not found',
+                    code: ApiErrorCode.PROVIDED_ID_NOT_FOUND
+                };
+                res.status(404).send(err);
+                return;
+            }
+
+            const labels = await dbQuery<ApiGetLabels>(async db => {
+                return db.collection(DBCollections.LABELS).find({ libraryId: lid }).toArray();
+            });
+            res.send(labels);
+        });
+    });
+
 
 }

fe5ff6b2d961a233ed5429952bc272bb2a4b5158
euberdeveloper <euberdeveloper@gmail.com>
2020-12-12 10:32:33 +0100

    refactor(types): refactor api types

--- src/types/users/registration/index.ts
+++
@@ -1,8 +0,0 @@
-import { DBUser } from '@/types/database/user';
-
-export type ApiPostUsersSignupBody = Omit<DBUser, '_id'>;
-
-export type ApiPostUsersVerifyTokenResult = { user: DBUser; token: string };
-
-export type ApiPostUsersLoginBody = { username: string; password: string };
-export type ApiPostUsersLoginResult = { user: DBUser; token: string };

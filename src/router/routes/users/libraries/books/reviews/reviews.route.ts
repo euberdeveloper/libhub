37ec62a443d0fc020d5845758f6d42a1a779d700
euberdeveloper <euberdeveloper@gmail.com>
2020-12-12 10:23:29 +0100

    feat(routes): add book reviews single get

--- src/router/routes/users/libraries/books/reviews/reviews.route.ts
+++ src/router/routes/users/libraries/books/reviews/reviews.route.ts
@@ -42,5 +42,40 @@ export function route(router: Router): void {
         });
     });
  
+    router.get('/users/:uid/libraries/:lid/books/:bid/reviews/:rid', auth('uid'), validateDbId(['lid', 'bid', 'rid']), async (req: Request & ReqAuthenticated & ReqIdParams, res) => {
+        await aceInTheHole(res, async () => {
+            const user = req.user;
+            const {lid, bid, rid} = req.idParams;
+
+            const found = await dbQuery<boolean>(async db => {
+                const library = await db.collection(DBCollections.LIBRARIES).countDocuments({ _id: lid, owners: user._id });
+                const book = await db.collection(DBCollections.BOOKS).countDocuments({ _id: bid, libraryId: lid });
+                return library > 0 && book > 0;
+            });
+            if (!found) {
+                const err: ApiError = {
+                    message: 'Library or book not found',
+                    code: ApiErrorCode.PROVIDED_ID_NOT_FOUND
+                };
+                res.status(404).send(err);
+                return;
+            }
+
+            const review = await dbQuery<ApiGetUsersUidLibrariesLidBooksBidReviewsRid>(async db => {
+                return db.collection(DBCollections.BOOK_REVIEWS).findOne({ _id: rid, bookId: bid });
+            });
+            if (!review) {
+                const err: ApiError = {
+                    message: 'Review not found',
+                    code: ApiErrorCode.PROVIDED_ID_NOT_FOUND
+                };
+                res.status(404).send(err);
+                return;
+            }
+
+            res.send(review);
+        });
+    });
+
 
 }

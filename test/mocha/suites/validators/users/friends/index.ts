67827c43195fba6dd018a44ff0207236aa635bfd
euberdeveloper <euberdeveloper@gmail.com>
2020-12-13 10:42:21 +0100

    test(mocha): add friends test

---
+++ test/mocha/suites/validators/users/friends/index.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+
+import { validateFriendRequest, validateFriendRequestResponse } from '../../../../../../src/router/routes/users/friends/utils';
+
+export default async function () {
+
+    describe('Validators and purgers of users/friends', async function () {
+
+        it('Should check the validateFriendRequest function accepts', async function () {
+            const bodyOk = {
+                message: 'tonono'
+            }
+            const ok = validateFriendRequest(bodyOk);
+            expect(ok).to.be.true;
+        });
+
+    });
+
+}
